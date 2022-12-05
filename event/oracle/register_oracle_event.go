be22a56f85d6d6c33ec4b8ff35122aeec4e5cdbb
Song Inchul <sic61695@gmail.com>
2022-12-05 18:01:55 +0900

    add TODO

--- event/oracle/register_oracle_event.go
+++ event/oracle/register_oracle_event.go
@@ -19,6 +19,7 @@ func (e RegisterOracleEvent) GetEventQuery() string {
 	return "message.action = 'RegisterOracle'"
 }
 
+// TODO: EventHandler for RegisterOracle will be implemented when RegisterOracle Tx implemented in panacea-core.
 func (e RegisterOracleEvent) EventHandler(event ctypes.ResultEvent) error {
 	panic("implement me")
 }
