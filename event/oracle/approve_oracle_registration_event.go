289e18c23b9b9d6876369c1fb18d3fed1a64a338
Song Inchul <sic61695@gmail.com>
2022-12-05 17:17:23 +0900

    add skeleton for event

--- event/oracle/approve_oracle_registration_event.go
+++ event/oracle/approve_oracle_registration_event.go
@@ -1,1 +1,24 @@
 package oracle
+
+import (
+	"github.com/medibloc/panacea-oracle/event"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
+)
+
+var _ event.Event = (*ApproveOracleRegistrationEvent)(nil)
+
+type ApproveOracleRegistrationEvent struct {
+	reactor event.Reactor
+}
+
+func NewApproveOracleRegistrationEvent(s event.Reactor) RegisterOracleEvent {
+	return RegisterOracleEvent{s}
+}
+
+func (e ApproveOracleRegistrationEvent) GetEventQuery() string {
+	return "message.action = 'ApproveOracleRegistration'"
+}
+
+func (e ApproveOracleRegistrationEvent) EventHandler(event ctypes.ResultEvent) error {
+	panic("implement me")
+}
--- event/oracle/register_oracle_event.go
+++ event/oracle/register_oracle_event.go
@@ -1,1 +1,24 @@
 package oracle
+
+import (
+	"github.com/medibloc/panacea-oracle/event"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
+)
+
+var _ event.Event = (*RegisterOracleEvent)(nil)
+
+type RegisterOracleEvent struct {
+	reactor event.Reactor
+}
+
+func NewRegisterOracleEvent(s event.Reactor) RegisterOracleEvent {
+	return RegisterOracleEvent{s}
+}
+
+func (e RegisterOracleEvent) GetEventQuery() string {
+	return "message.action = 'RegisterOracle'"
+}
+
+func (e RegisterOracleEvent) EventHandler(event ctypes.ResultEvent) error {
+	panic("implement me")
+}
