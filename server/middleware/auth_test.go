8de2d47f884e3411fdbcb68362aca1a6ec19b228
gyuguen <gyuguen.jang@medibloc.org>
2022-12-15 12:41:27 +0900

    fix

--- server/middleware/auth_test.go
+++ server/middleware/auth_test.go
@@ -18,14 +18,13 @@ import (
 	"github.com/lestrrat-go/jwx/v2/jwt"
 	datadealtypes "github.com/medibloc/panacea-core/v2/x/datadeal/types"
 	oracletypes "github.com/medibloc/panacea-core/v2/x/oracle/types"
-	"github.com/medibloc/panacea-oracle/crypto"
 	"github.com/medibloc/panacea-oracle/server/middleware"
 	"github.com/stretchr/testify/require"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 var (
-	testPrivKey, _ = crypto.GeneratePrivateKeyFromMnemonic("", 371, 0, 0)
+	testPrivKey = secp256k1.GenPrivKey()
 	testAccAddr = "test-addr"
 
 	testHandler = middleware.NewJWTAuthMiddleware(&mockQueryClient{}).Middleware(
@@ -82,10 +81,7 @@ func TestInvalidJWT(t *testing.T) {
 }
 
 func TestAccountNotFound(t *testing.T) {
-	priv := &secp256k1.PrivKey{
-		Key: testPrivKey.Bytes(),
-	}
-	jwt := testGenerateJWT(t, "dummy-account", priv, 10*time.Second)
+	jwt := testGenerateJWT(t, "dummy-account", testPrivKey, 10*time.Second)
 	testHTTPRequest(
 		t,
 		fmt.Sprintf("Bearer %s", string(jwt)),
