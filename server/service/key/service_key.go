46120aec4fb4348a3ac829e4d4ffc2f0b8840b72
gyuguen <gyuguen.jang@medibloc.org>
2023-01-31 14:50:27 +0900

    fix

--- server/service/key/service_key.go
+++ server/service/key/service_key.go
@@ -27,26 +27,26 @@ func (s *combinedKeyService) GetSecretKey(ctx context.Context, req *key.GetSecre
 
 	deal, err := queryClient.GetDeal(ctx, dealID)
 	if err != nil {
-		return nil, fmt.Errorf("%w", err)
+		return nil, fmt.Errorf("failed to get deal(%d): %w", dealID, err)
 	}
 
 	if requesterAddress != deal.ConsumerAddress {
-		return nil, fmt.Errorf("%w", err)
+		return nil, fmt.Errorf("only consumer request secret key")
 	}
 
 	_, err = queryClient.GetConsent(ctx, dealID, dataHashStr)
 	if err != nil {
-		return nil, fmt.Errorf("%w", err)
+		return nil, fmt.Errorf("failed to get consent(dealID: %d, dataHash %s). %w", dealID, dataHashStr, err)
 	}
 
 	consumerAcc, err := queryClient.GetAccount(ctx, deal.ConsumerAddress)
 	if err != nil {
-		return nil, fmt.Errorf("%w", err)
+		return nil, fmt.Errorf("failed to get consumer account: %w", err)
 	}
 	consumerPubKeyBz := consumerAcc.GetPubKey().Bytes()
 	consumerPubKey, err := btcec.ParsePubKey(consumerPubKeyBz, btcec.S256())
 	if err != nil {
-		return nil, fmt.Errorf("%w", err)
+		return nil, fmt.Errorf("failed to parse consumer public key: %w", err)
 	}
 
 	sharedKey := crypto.DeriveSharedKey(oraclePrivKey, consumerPubKey, crypto.KDFSHA256)
@@ -54,7 +54,7 @@ func (s *combinedKeyService) GetSecretKey(ctx context.Context, req *key.GetSecre
 	secretKey := GetCombinedKey(oraclePrivKey.Serialize(), dealID, req.DataHash)
 	encryptedSecretKey, err := crypto.Encrypt(sharedKey, nil, secretKey)
 	if err != nil {
-		return nil, fmt.Errorf("%w", err)
+		return nil, fmt.Errorf("failed to encrypt secret key with shared key: %w", err)
 	}
 
 	return &key.GetSecretKeyResponse{
