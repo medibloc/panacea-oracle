d6968a5cb82452933c3ff45fe671424d5cf1362d
gyuguen <gyuguen.jang@medibloc.org>
2023-02-17 15:42:31 +0900

    remove server service

--- server/service/key/service.go
+++ server/service/key/service.go
@@ -5,18 +5,19 @@ import (
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	key "github.com/medibloc/panacea-oracle/pb/key/v0"
-	serverservice "github.com/medibloc/panacea-oracle/server/service"
+	"github.com/medibloc/panacea-oracle/service"
 	"google.golang.org/grpc"
 )
 
-var _ serverservice.Service = &secretKeyService{}
+var _ service.Service = &secretKeyService{}
 
 type secretKeyService struct {
 	key.UnimplementedKeyServiceServer
-	serverservice.Service
+
+	service.Service
 }
 
-func RegisterService(svc serverservice.Service, svr *grpc.Server) {
+func RegisterService(svc service.Service, svr *grpc.Server) {
 	key.RegisterKeyServiceServer(svr, &secretKeyService{
 		Service: svc,
 	})
