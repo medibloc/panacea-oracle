ed0345ca550ef38882057dc7a81b53fa5633c846
InChul <49394875+inchori@users.noreply.github.com>
2023-02-22 18:31:04 +0900

    fix

--- server/service/datadeal/service_data_validation.go
+++ server/service/datadeal/service_data_validation.go
@@ -88,9 +88,15 @@ func (s *dataDealServiceServer) ValidateData(ctx context.Context, req *datadeal.
 		return nil, fmt.Errorf("data hash mismatch")
 	}
 
-	if err := validation.ValidateJSONSchemata(decryptedData, deal.DataSchema); err != nil {
-		log.Debugf("failed to validate data: %s", err.Error())
-		return nil, fmt.Errorf("failed to validate data")
+	//if err := validation.ValidateJSONSchemata(decryptedData, deal.DataSchema); err != nil {
+	//	log.Debugf("failed to validate data: %s", err.Error())
+	//	return nil, fmt.Errorf("failed to validate data")
+	//}
+	//
+
+	if err := validation.ValidateVerifiablePresentation(decryptedData, providerPubKeyBytes); err != nil {
+		log.Errorf("failed to validate verifiable presentation: %s", err.Error())
+		return nil, fmt.Errorf("failed to validate VP")
 	}
 
 	// Re-encrypt data using a combined key
