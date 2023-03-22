a7b186ddf963398ed64f3415420edd8b8214573d
gyuguen <gyuguen.jang@medibloc.org>
2023-03-22 17:38:33 +0900

    fix

--- validation/verifiable_presentation.go
+++ validation/verifiable_presentation.go
@@ -2,6 +2,7 @@ package validation
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/hyperledger/aries-framework-go/pkg/doc/did"
 
@@ -10,12 +11,16 @@ import (
 	"github.com/medibloc/vc-sdk/pkg/vc"
 )
 
+var mutex = &sync.Mutex{}
+
 type didResolver interface {
 	Resolve(did string, opts ...vdr.DIDMethodOption) (*did.DocResolution, error)
 }
 
 // ValidateVP validates verifiable presentation
 func ValidateVP(vdr didResolver, vpBytes, pdBytes []byte) error {
+	mutex.Lock()
+	defer mutex.Unlock()
 	f, err := vc.NewFramework(vdr)
 	if err != nil {
 		return fmt.Errorf("failed to create a framework for VP verification: %w", err)
